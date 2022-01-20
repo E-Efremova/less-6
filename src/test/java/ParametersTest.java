dc5b0ee294dbb758afc5b3161c2dde080ad22c66
e.efremova <e.efremova@dextechnology.com>
2022-01-20 09:40:03 +0200

    Привязка Allure отчетов

---
+++ src/test/java/ParametersTest.java
@@ -0,0 +1,12 @@
+import io.qameta.allure.Allure;
+import org.junit.jupiter.api.Test;
+
+public class ParametersTest {
+
+    @Test
+    public void lambdaLabelsTest() {
+        Allure.parameter("Регион", "Москва");
+        Allure.parameter("Область", "Московская");
+    }
+
+}
